9c7e3addbed065ce5754107c44e8f9aff2de1d31
dhagandev <dhagan@utexas.edu>
2019-06-13 22:50:30 -0500

    Set up individual gem page:

--- collectorapp/main_app/models.py
+++ collectorapp/main_app/models.py
@@ -2,16 +2,18 @@ from django.db import models
 import datetime
 
 # Create your models here.
-# class Gem:
-# 	def __init__(self, gem_type, color, location_found, date_found):
-# 		self.gem_type = gem_type
-# 		self.color = color
-# 		self.location_found = location_found
-# 		self.date_found = date_found
+class Gem(models.Model):
+	gem_type = models.CharField(max_length=100)
+	color = models.CharField(max_length=100)
+	location_found = models.CharField(max_length=100)
+	date_found = models.DateField()
 
-# gems = [
-# 	Gem('Turquoise', 'Light Blue', 'Arizona', datetime.datetime(2019, 6, 9)),
-# 	Gem('Quartz', 'Rose Pink', 'South Dakota', datetime.datetime(2019, 5, 23)),
-# 	Gem('Ruby', 'Dark Red', 'Brazil', datetime.datetime(2019, 5, 7)),
-# 	Gem('Malachite', 'Green', 'Mexico', datetime.datetime(2019, 4, 16))
-# ]
+	def __str__(self):
+		return self.color + " " + self.gem_type
+
+gems = [
+	Gem('Turquoise', 'Light Blue', 'Arizona', datetime.datetime(2019, 6, 9)),
+	Gem('Quartz', 'Rose Pink', 'South Dakota', datetime.datetime(2019, 5, 23)),
+	Gem('Ruby', 'Dark Red', 'Brazil', datetime.datetime(2019, 5, 7)),
+	Gem('Malachite', 'Green', 'Mexico', datetime.datetime(2019, 4, 16))
+]
